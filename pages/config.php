a61b6be4a723278be5cc6fc760ca8cedccfcfea1
MD. Rahat <60851464+MDRTHW@users.noreply.github.com>
2022-05-28 18:54:53 +0600

    first commit

---
+++ pages/config.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+$dbHost='localhost';
+$dbName='kosai_limited_users';
+$dbUsername='root';
+$dbPassword='';
+
+$dbc=mysqli_connect($dbHost,$dbUsername,$dbPassword,$dbName);
+
+?>
---
+++ pages/sign_up.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sign up in Kosai Limited</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  </head>
+  <body>
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+            <!--useless-->
+            </div>
+            <div class="col-sm-4">
+            <center><img src="assets/home-logo.png" alt="Kosai Limited Logo" style="width:70;height:70px;" class="logo img-fluid mb-2">
+                    <h1 class="mb-3">Kosai Limited</h1></center>
+            </div>
+            <div class="col-sm-4">
+                <!--useless-->
+            </div>
+        </div>
+
+    <div class="row">
+        <div class="col-sm-4">
+
+        </div>
+        <div class="col-sm-4">
+        <div class="signup_form">
+            <form action="" method="POST">
+                <div class="mb-3">
+                    <label class="label_txt">Username or Email</label>
+                    <input type="email" class="form-control" >
+                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" >
+                </div>
+                <button type="submit" class="form-btn btn btn-primary">Login</button>
+            </form>
+        </div>
+        <div class="col-sm-4">
+            
+        </div>
+    </div>
+
+
+
+
+    </div>
+
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+  </body>
+</html>
